24eebe0cf89aec084948581830fd83d3c326bfa0
Lauri Niskanen <ape@ape3000.com>
2016-04-24 21:16:07 +0300

    Use player colors on sprites

--- src/sprites/EntitySprite.js
+++ src/sprites/EntitySprite.js
@@ -1,4 +1,6 @@
+import _ from 'lodash';
 import Phaser from 'phaser'
+import {store} from '../ui/App.js'
 
 export default class extends Phaser.Sprite {
 
@@ -46,37 +48,49 @@ export default class extends Phaser.Sprite {
       const y = entitySprite.y;
       this.onMoveRequest({id: entitySprite.entity.id, pos: {x, y}});
     }, this);
+
+    // TODO: Only update color when store->players changes
+    store.subscribe(() => { this.updateColor() });
   }
 
-  update () {
-    if (this.entity.selectedClientId === this.clientId) {
-      this.tint = 0xccffff;
-    } else if (this.entity.selectedClientId !== null) {
-      this.tint = 0xffccff;
-    } else {
-      this.tint = 0xffffff;
-    }
-  }
+  update() {}
 
   updateEntity(entity) {
     Object.assign(this.entity, entity);
+
     if (entity.pos) {
       this.tween = this.game.add.tween(this).to(this.entity.pos, 300);
       this.tween.start();
     }
-    
-    if (this.entity.selectedClientId === this.clientId) {
-      this.input.enableDrag();
-      if (this.pointerDown) {
-        this.x += this.pointerDown.pointer.x - this.pointerDown.x;
-        this.y += this.pointerDown.pointer.y - this.pointerDown.y;
-        this.input.startDrag(this.pointerDown.pointer);
+
+    if (_.has(entity, 'selectedClientId')) {
+      this.updateColor();
+
+      if (this.entity.selectedClientId === this.clientId) {
+        this.input.enableDrag();
+        if (this.pointerDown) {
+          this.x += this.pointerDown.pointer.x - this.pointerDown.x;
+          this.y += this.pointerDown.pointer.y - this.pointerDown.y;
+          this.input.startDrag(this.pointerDown.pointer);
+        }
+      } else {
+        this.input.disableDrag();
       }
-    } else {
-      this.input.disableDrag();
     }
 
     this.z = this.entity.depth;
   }
 
+  updateColor() {
+    if (this.entity.selectedClientId !== null) {
+      const player = store.getState().players[this.entity.selectedClientId];
+      if (player) {
+        this.tint = Phaser.Color.HSLtoRGB(player.hue, 1.0, 0.9).color;
+      } else {
+        this.tint = 0xcccccc;
+      }
+    } else {
+      this.tint = 0xffffff;
+    }
+  }
 }
