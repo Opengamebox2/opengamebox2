340cfa34d08f3d38b08191bfe38c00b57dd7ebe4
Ville Nukarinen <ville.nukarinen@gmail.com>
2016-04-25 16:25:36 +0300

    Persist image hashes to settings

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
 
-function settingsReducer(state = {}, action) {
+function settingsReducer(state = {
+  images: {}
+}, action) {
   Object.freeze(state);
   switch (action.type) {
     case 'PLAYER_UPDATE_REQUEST': {
       return _.assign({}, state, action.data);
+    }
+
+    case 'ENTITY_CREATE': {
+      const images = _.clone(state.images);
+      action.data.forEach(entity => {
+        if (!images[entity.imgHash]) {
+        images[entity.imgHash] = {};
+        }
+      });
+      return _.assign({}, state, {images});
     }
 
     default:
