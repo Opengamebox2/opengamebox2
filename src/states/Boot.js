b8f953b74cd23ab4b7403f31bdd234b3d57c135f
Ville Nukarinen <ville.nukarinen@gmail.com>
2016-04-25 19:16:33 +0300

    Change canvas background color

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -3,7 +3,7 @@ import WebFont from 'webfontloader'
 
 export default class extends Phaser.State {
   init () {
-    this.stage.backgroundColor = '#EDEEC9'
+    this.stage.backgroundColor = '#222222'
     this.fontsReady = false
     this.fontsLoaded = this.fontsLoaded.bind(this)
   }
